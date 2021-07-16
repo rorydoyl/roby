6255cdaf958fade3f55fb81f6992c12d42f61e5a
matlo <write@matlo.me>
2021-07-16 11:41:58 +0100

    stats

--- bot.js
+++ bot.js
@@ -1,208 +1,249 @@
-const Discord = require('discord.js')
+const Discord = require("discord.js")
 const client = new Discord.Client()
 const funcs = require("./functions.js")
 const cookies = require("./cookies.js")
-const { GoogleSpreadsheet } = require('google-spreadsheet')
+const { GoogleSpreadsheet } = require("google-spreadsheet")
 
-var express = require("express");
-var app = express();
+var express = require("express")
+var app = express()
 
 app.listen(3344, () => {
-    console.log("Server running on port 3344");
-   });
+  console.log("Server running on port 3344")
+})
 
 app.get("/roby", (req, res, next) => {
-    res.send("<h1>ciao pierre!</h1>")
-    pierre()
-});
+  res.send("<h1>ciao pierre!</h1>")
+  pierre()
+})
 
 app.get("/insta", (req, res, next) => {
-    res.send("<h1>🤖 on it!</h1>")
-    insta()
-});
+  res.send("<h1>🤖 on it!</h1>")
+  insta()
+})
 
 // used to read google spreadsheet
 // https://theoephraim.github.io/node-google-spreadsheet/
-const creds = require('./secrets/roby-google-key.json') 
-const doc = new GoogleSpreadsheet('1ek6o7DY5m-4yxdkA8RQJNPY2n-0T1TR7BbP1i23zirQ')
+const creds = require("./secrets/roby-google-key.json")
+const doc = new GoogleSpreadsheet(
+  "1ek6o7DY5m-4yxdkA8RQJNPY2n-0T1TR7BbP1i23zirQ"
+)
 
 // pierre()
 
-
 //ask @matlo for the config.json file
-const { prefix, token } = require('./secrets/config.json')
+const { prefix, token } = require("./secrets/config.json")
+
 client.login(token)
 
-client.on('ready', () => {    
-    console.log(`Logged in as ${client.user.tag}!`)    
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.tag}!`)
 })
 
-client.on('message', async msg => {
-    
-    // so it's case agnostic
-    const command = msg.content.toLowerCase()    
+client.on("message", async (msg) => {
+  // so it's case agnostic
+  const command = msg.content.toLowerCase()
 
-    //help
-    if (command === `${prefix}help`) {    
+  //help
+  if (command === `${prefix}help`) {
+    const embed = new Discord.MessageEmbed()
+    // Set the title of the field
+    embed.setTitle("🤖  Roby ")
+    // color of the sidebar
+    embed.setColor(0x0000ff)
+    // Set the main content of the embed
+    embed.setDescription("Full command list")
+    embed.addField("📡 Radio Listeners", "`roby listeners pls`", true)
+    embed.addField("🧙‍♂️ Wisdom", "`roby wisdom pls`", true)
+    embed.addField("💰 BTC", "`roby btc pls`", true)
+    embed.addField("💰 DOT", "`roby polka pls`", true)
+    embed.addField("( ° ͜ʖ °)", "`roby lenny face pls`", true)
+    embed.addField("👋 Hey", "`roby what's up`", true)
+    embed.addField("🏓 Ping", "`ping`", true)
+    embed.addField("🧙‍♂️ Random Poem", "`roby poem pls`", true)
+    // Send the embed to the same channel as the message
+    msg.channel.send(embed)
+  }
+
+  // oio radio listeners
+  if (command === `${prefix}listeners pls`) {
+    await funcs.getListeners().then((result) => {
+      if (typeof result == "number")
+        msg.channel.send("📡 oio.radio has " + result + " listeners")
+      else msg.channel.send(result)
+    })
+  }
+
+  // wisdom
+  if (command === `${prefix}wisdom pls`) {
+    msg.channel.send("🧙‍♂️ " + cookies.getFortune())
+  }
+
+  // feedback wdyt
+  if (command === `${prefix}wdyt`) {
+    msg.channel.send(cookies.getFeedback())
+  }
+
+  // Website Stats
+  if (command === `${prefix}stats pls`) {
+    const domains = ["oio.studio", "oio.land", "bouncing.band"]
+    domains.forEach(async (domain) => {
+      await funcs.getStats(domain).then((result) => {
+        //unpack
+        const res = result.results
+        const bounceRate = res.bounce_rate.value + "%"
+        const pageViews = res.pageviews.value + " views"
+        const visitDuration = res.visit_duration.value + " minutes"
+        const visitors = res.visitors.value + " users"
 
         const embed = new Discord.MessageEmbed()
         // Set the title of the field
-        embed.setTitle('🤖  Roby ') 
-        // color of the sidebar       
+        embed.setTitle(`🌐  ${domain} `)
+        // color of the sidebar
         embed.setColor(0x0000ff)
         // Set the main content of the embed
-        embed.setDescription('Full command list');
-        embed.addField("📡 Radio Listeners","`roby listeners pls`",true) 
-        embed.addField("🧙‍♂️ Wisdom","`roby wisdom pls`",true) 
-        embed.addField("💰 BTC","`roby btc pls`",true) 
-        embed.addField("💰 DOT","`roby polka pls`",true) 
-        embed.addField("( ° ͜ʖ °)","`roby lenny face pls`",true) 
-        embed.addField("👋 Hey","`roby what's up`",true)         
-        embed.addField("🏓 Ping","`ping`",true)
-        embed.addField("🧙‍♂️ Random Poem","`roby poem pls`",true);         
+        embed.setDescription("In the last 24 hours")
+        embed.addField(`🕺 Visitors`, visitors, false)
+        embed.addField(`🏀 Bounce Rate`, bounceRate, false)
+        embed.addField(`👀 Page Views`, pageViews, false)
+        embed.addField(`⏱ Visit Duration`, visitDuration, false)
         // Send the embed to the same channel as the message
-        msg.channel.send(embed);
-    }   
+        msg.channel.send(embed)
+      })
+    })
+  }
 
-    // oio radio listeners
-    if (command === `${prefix}listeners pls`) {
-        await funcs.getListeners().then((result) => {            
-            if (typeof result == "number")
-                msg.channel.send("📡 oio.radio has " + result + " listeners")
-            else
-                msg.channel.send(result)
-        })
-    }
-    
-    // wisdom
-    if (command === `${prefix}wisdom pls`) {
-        msg.channel.send("🧙‍♂️ " + cookies.getFortune())
-    }
+  // BTC value
+  if (command === `${prefix}btc pls`) {
+    await funcs.getBTC().then((result) => {
+      let str = result.toFixed(2) + "$"
+      msg.channel.send(" 1 BTC = **" + str + "**")
+    })
+  }
 
-    // feedback wdyt
-    if (command === `${prefix}wdyt`) {
-        msg.channel.send(cookies.getFeedback())
-    }
+  // DOT value
+  if (command === `${prefix}polka pls`) {
+    await funcs.getDOT().then((result) => {
+      let str = result.toFixed(2) + "$"
+      msg.channel.send(" 1 DOT = **" + str + "**")
+    })
+  }
 
-    // BTC value
-    if (command === `${prefix}btc pls`) {
-        await funcs.getBTC().then((result) => {           
-            let str = result.toFixed(2)+"$" 
-            msg.channel.send(" 1 BTC = **" + str +"**")        
-        })
-    }
+  // lenny face
+  if (command === `${prefix}lenny face pls`) {
+    msg.channel.send("`( ° ͜ʖ °)`")
+  }
 
-    // BTC value
-    if (command === `${prefix}polka pls`) {
-        await funcs.getDOT().then((result) => {           
-            let str = result.toFixed(2)+"$" 
-            msg.channel.send(" 1 DOT = **" + str +"**")        
-        })
-    }
+  // what's up
+  if (command === `${prefix}what's up` || command === `${prefix}what’s up`) {
+    msg.channel.send("👋 " + msg.author.username + ", " + cookies.getCheers())
+  }
 
-    // lenny face
-    if (command === `${prefix}lenny face pls`) {        
-        msg.channel.send("`( ° ͜ʖ °)`")
-    }
+  // real?
+  if (command === `${prefix}are you real?`) {
+    msg.channel.send("idk, are you real?")
+  }
 
-    // what's up
-    if (command === `${prefix}what's up` || command === `${prefix}what’s up`) {            
-        msg.channel.send("👋 " + msg.author.username + ", " + cookies.getCheers())
-      }
-    
-    // real?
-    if (command === `${prefix}are you real?`) {
-        msg.channel.send("idk, are you real?")
-      }
+  // ping
+  if (command === `ping`) {
+    msg.channel.send("🏓 pong")
+  }
 
-    // ping
-    if (command === `ping`) {            
-        msg.channel.send("🏓 pong")
-    }
+  // pong
+  if (command === `pong`) {
+    msg.channel.send("🤖 can't compute")
+    msg.channel.send("*[fatal error - roby is restarting]*")
+    msg.channel.send("...")
+  }
 
-    // pong
-    if (command === `pong`) {            
-        msg.channel.send("🤖 can't compute")
-        msg.channel.send("*[fatal error - roby is restarting]*")
-        msg.channel.send("...")
-    }
+  // beep
+  if (command === `beep`) {
+    msg.channel.send("🤖 bop")
+  }
 
-    // beep
-    if (command === `beep`) {            
-        msg.channel.send("🤖 bop")
-    }
+  // bop
+  if (command === `bop`) {
+    msg.channel.send("what?")
+  }
 
-    // bop
-    if (command === `bop`) {            
-        msg.channel.send("what?")
-    }
+  // poem
+  // lenny face
+  if (command === `${prefix}poem pls`) {
+    msg.channel.send(await func.getRandomPoem())
+  }
 
-    // poem
-    // lenny face
-    if (command === `${prefix}poem pls`) {        
-        msg.channel.send( await func.getRandomPoem() );
-    }
+  // thanks
+  if (command === `thanks roby`) {
+    msg.reply("you are welcome")
+  }
+  if (command === `thank you roby`) {
+    msg.reply("you are welcome")
+  }
 
-    // thanks
-    if (command === `thanks roby`) {            
-        msg.reply("you are welcome")
-    }    
-    if (command === `thank you roby`) {            
-        msg.reply("you are welcome")
-    }
-  })
+  // pixels only police
+  // if (msg.channel.name === "🔸pixels-only") {
+  //     if (msg.attachments.size === 0) {
 
-
-
+  //         // msg.channel.send(" :police_car: Only pixels allowed in this channel :police_car: ")
+  //     }
+  // }
+})
 
 ///// FUNCTIONS
 
 async function pierre() {
-    await doc.useServiceAccountAuth(creds)
-    await doc.loadInfo(); 
-    const sheet = doc.sheetsByIndex[0]; // or use doc.sheetsById[id]
-    const rows = await sheet.getRows(); // can pass in { limit, offset }
-    
-    const channel = client.channels.cache.find(channel => channel.name === '🔥cool-stuff')    
+  await doc.useServiceAccountAuth(creds)
+  await doc.loadInfo()
+  const sheet = doc.sheetsByIndex[0] // or use doc.sheetsById[id]
+  const rows = await sheet.getRows() // can pass in { limit, offset }
 
-    const embed = new Discord.MessageEmbed()
-    // Set the title of the field
-    embed.setTitle("🗞 New Pierre's Newsletter! ") 
-    embed.setDescription("Your weekly digest of cool stuff") 
-    
-    // color of the sidebar       
-    embed.setColor(0x7FFFD4)             
+  const channel = client.channels.cache.find(
+    (channel) => channel.name === "🔥cool-stuff"
+  )
 
-    let testa = "_  _"
-    let corpo = "_  _"
-    embed.addField("_  _", "_  _" ,false)             
-    rows.map((row, i) => {
-        if (row.header !== undefined && row.header !== "") {                        
-            if (i !== 0) {
-                corpo += "\n _  _ \n"
-                embed.addField(funcs.posterize(testa), corpo ,false)             
-            }
-            testa = row.header            
-            corpo = ""
-        } else {   
-            s = "[🔸 "+row.text+"]("+row.link+") \n"
-            corpo += s            
-        }
-    })    
+  const embed = new Discord.MessageEmbed()
+  // Set the title of the field
+  embed.setTitle("🗞 New Pierre's Newsletter! ")
+  embed.setDescription("Your weekly digest of cool stuff")
 
-    embed.addField("To get Pierre's Newsletter in your inbox ", "[👉 SUBSCRIBE HERE](http://eepurl.com/gdPSM1)" ,false)     
-    embed.addField("_  _", "_  _" ,false)             
-    embed.addField("_Thank you Pierre!_", "_  _" ,false)             
+  // color of the sidebar
+  embed.setColor(0x7fffd4)
 
-    channel.send(embed)        
+  let testa = "_  _"
+  let corpo = "_  _"
+  embed.addField("_  _", "_  _", false)
+  rows.map((row, i) => {
+    if (row.header !== undefined && row.header !== "") {
+      if (i !== 0) {
+        corpo += "\n _  _ \n"
+        embed.addField(funcs.posterize(testa), corpo, false)
+      }
+      testa = row.header
+      corpo = ""
+    } else {
+      s = "[🔸 " + row.text + "](" + row.link + ") \n"
+      corpo += s
+    }
+  })
+
+  embed.addField(
+    "To get Pierre's Newsletter in your inbox ",
+    "[👉 SUBSCRIBE HERE](http://eepurl.com/gdPSM1)",
+    false
+  )
+  embed.addField("_  _", "_  _", false)
+  embed.addField("_Thank you Pierre!_", "_  _", false)
+
+  channel.send(embed)
 }
 
-async function insta () {      
-    await funcs.insta().then((post) => {
-        const channel = client.channels.cache.find(channel => channel.name === '🤖bots')    
-        channel.send("✨ I just did an instagram post, check it out")        
-        channel.send("👉 "+post.url)        
-        channel.send("------------------------------")                
-        channel.send("_ " + post.caption + " _", {files: [post.imageUrl]})                
-    })
+async function insta() {
+  await funcs.insta().then((post) => {
+    const channel = client.channels.cache.find(
+      (channel) => channel.name === "🤖bots"
+    )
+    channel.send("✨ I just did an instagram post, check it out")
+    channel.send("👉 " + post.url)
+    channel.send("------------------------------")
+    channel.send("_ " + post.caption + " _", { files: [post.imageUrl] })
+  })
 }
--- functions.js
+++ functions.js
@@ -2,90 +2,107 @@ const axios = require("axios")
 const cheerio = require("cheerio")
 const userInstagram = require("user-instagram")
 
-let price = require('crypto-price')
-const { GoogleSpreadsheet } = require('google-spreadsheet')
+let price = require("crypto-price")
+const { GoogleSpreadsheet } = require("google-spreadsheet")
+const { plausibleToken } = require("./secrets/config.json")
 
-const randomPoemUrl = "https://tofu.wtf/poems/api/random";
+const randomPoemUrl = "https://tofu.wtf/poems/api/random"
 
 // used to read google spreadsheet
 // https://theoephraim.github.io/node-google-spreadsheet/
-const creds = require('./secrets/roby-google-key.json') 
-const doc = new GoogleSpreadsheet('1ek6o7DY5m-4yxdkA8RQJNPY2n-0T1TR7BbP1i23zirQ')
+const creds = require("./secrets/roby-google-key.json")
+const doc = new GoogleSpreadsheet(
+  "1ek6o7DY5m-4yxdkA8RQJNPY2n-0T1TR7BbP1i23zirQ"
+)
 
 const fetchData = async (site) => {
-    const result = await axios.get(site)
-    return cheerio.load(result.data)
+  const result = await axios.get(site)
+  return cheerio.load(result.data)
 }
 
 exports.getBTC = async () => {
-    const btc = await price.getCryptoPrice("USD", "BTC").then(obj => {         
-        return parseFloat(obj.price)        
-    }).catch(err => {
-        console.log(err)
-    })      
-    return btc
+  const btc = await price
+    .getCryptoPrice("USD", "BTC")
+    .then((obj) => {
+      return parseFloat(obj.price)
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+  return btc
 }
 
 exports.getDOT = async () => {
-  const dot = await price.getCryptoPrice("USD", "DOT").then(obj => {         
-      return parseFloat(obj.price)        
-  }).catch(err => {
+  const dot = await price
+    .getCryptoPrice("USD", "DOT")
+    .then((obj) => {
+      return parseFloat(obj.price)
+    })
+    .catch((err) => {
       console.log(err)
-  })      
+    })
   return dot
 }
 
 exports.getListeners = async () => {
-    const www = "https://stream.oio.radio/"
-    const $ = await fetchData(www)
-    let listeners = "🔇 oio.radio is off mate!"
-    $('.streamstats').each(function( index ) {
-        if (index === 1)
-            listeners = parseInt($( this ).text())
-    })
-    return listeners
+  const www = "https://stream.oio.radio/"
+  const $ = await fetchData(www)
+  let listeners = "🔇 oio.radio is off mate!"
+  $(".streamstats").each(function (index) {
+    if (index === 1) listeners = parseInt($(this).text())
+  })
+  return listeners
 }
 
-exports.insta = async () => {    
-  // Gets informations about a user  
-  const url = await userInstagram('roby.oio') // Same as getUserData()  
-  .then(res => {        
-    return res.posts[0]
-  })
-  .catch(console.error);
-  return url  
+exports.insta = async () => {
+  // Gets informations about a user
+  const url = await userInstagram("roby.oio") // Same as getUserData()
+    .then((res) => {
+      return res.posts[0]
+    })
+    .catch(console.error)
+  return url
 }
 
 exports.getRandomPoem = async () => {
-    try {
-        const response = await axios.get( randomPoemUrl );
-        return response.data.content;
-    }
-    catch (_) {
-        return "😢 sorry, no poem 😢"
-    }
+  try {
+    const response = await axios.get(randomPoemUrl)
+    return response.data.content
+  } catch (_) {
+    return "😢 sorry, no poem 😢"
+  }
 }
 
-exports.posterize = (wordLetters) => { 
-    let num = /([0-9])/;
-    let alph = /([a-z])/;
-    let string = [];
-    Array.from(wordLetters).map((wordSet, i1) => {
-      Array.from(wordSet).map((word, i2) => {
-        if (word === ' ') {
-          string.push(`:small_blue_diamond:`);
-        } else if (alph.test(word)) {
-          string.push(`:regional_indicator_${word}:`);
-        } else if (num.test(word)) {
-          for (var i = 0; i < digits.length; i++) {
-            if (digits[i] === word) {
-              string.push(`:${numbers[i]}:`);
-              break;
-            }
+exports.posterize = (wordLetters) => {
+  let num = /([0-9])/
+  let alph = /([a-z])/
+  let string = []
+  Array.from(wordLetters).map((wordSet, i1) => {
+    Array.from(wordSet).map((word, i2) => {
+      if (word === " ") {
+        string.push(`:small_blue_diamond:`)
+      } else if (alph.test(word)) {
+        string.push(`:regional_indicator_${word}:`)
+      } else if (num.test(word)) {
+        for (var i = 0; i < digits.length; i++) {
+          if (digits[i] === word) {
+            string.push(`:${numbers[i]}:`)
+            break
           }
         }
-      });
-    });    
-    return (string.join(""))
+      }
+    })
+  })
+  return string.join("")
 }
 
+exports.getStats = async (domain) => {
+  const URL = `https://plausible.io/api/v1/stats/aggregate?site_id=${domain}&period=day&metrics=visitors,pageviews,bounce_rate,visit_duration`
+  const response = await fetch(URL, {
+    headers: {
+      Authorization: `Bearer ${plausibleToken}`,
+    },
+  })
+  const data = await response.json()
+  return data
+}
